3c3a6a4307e57c970389fd8b04aac83ca4fc7354
Basheerbachi <85440600+Basheerbachi@users.noreply.github.com>
2022-03-07 18:32:52 +0530

    Add files via upload

---
+++ java_code/script.js
@@ -0,0 +1,53 @@
+let age = 49;
+switch(true){
+    case age<18:
+        console.log("child");
+        break;
+    case 18<age<40:
+        console.log("Adult");
+        break;
+    default:
+        console.log("old");
+        break;
+}
+let gen="m"
+switch(gender){
+	case "m":
+	    console.log("male") ;
+	    break;
+	case "m":
+	   console.log("famale");
+	   break;
+	default:
+	     console.log("trans-gender") ;
+         break;
+}
+let day=2;
+switch(day){
+    case day=1:
+        console.log("Monday");
+        break;
+    case day=2:
+        console.log("Tuesday");
+        break;
+    case day=3:
+        console.log("Wednesday");
+        break;
+    case day=2:
+        console.log("Thursday");
+            break;
+    case day=3:
+        console.log("Friday");
+        break;
+    case day=3:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Friday");
+    }
+
+
+for(let i=1; i<=10; i++){
+    console.log(19*i);
+}
+
